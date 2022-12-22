d3e0c8ff196d6122146ebacab6d2c13f367dfbbb
Deepin Developer <deepin-dev@deepin.org>
2022-12-22 15:25:42 +0800

    feat: Init commit
    init openrc
    
    Log:

---
+++ src/librc/librc.h
@@ -0,0 +1,59 @@
+/*
+ * librc.h
+ * Internal header file to setup build env for files in librc.so
+ */
+
+/*
+ * Copyright (c) 2007-2015 The OpenRC Authors.
+ * See the Authors file at the top-level directory of this distribution and
+ * https://github.com/OpenRC/openrc/blob/master/AUTHORS
+ *
+ * This file is part of OpenRC. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this
+ * distribution and at https://github.com/OpenRC/openrc/blob/master/LICENSE
+ * This file may not be copied, modified, propagated, or distributed
+ *    except according to the terms contained in the LICENSE file.
+ */
+
+#ifndef _LIBRC_H_
+#define _LIBRC_H_
+
+#define _IN_LIBRC
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <limits.h>
+#include <paths.h>
+#include <regex.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <time.h>
+#include <unistd.h>
+
+#if defined(BSD) && !defined(__GNU__)
+#include <sys/param.h>
+#include <sys/user.h>
+#include <sys/sysctl.h>
+#include <kvm.h>
+#else
+#include <sys/param.h>
+#endif
+
+#include "rc.h"
+#include "rc-misc.h"
+
+#endif
---
+++ src/rc/rc-sysvinit.c
@@ -0,0 +1,102 @@
+/*
+ * rc-sysvinit.c
+ * Helper to send a runlevel change to sysvinit
+ */
+
+/*
+ * Copyright (c) 2019 The OpenRC Authors.
+ * See the Authors file at the top-level directory of this distribution and
+ * https://github.com/OpenRC/openrc/blob/master/AUTHORS
+ *
+ * This file is part of OpenRC. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this
+ * distribution and at https://github.com/OpenRC/openrc/blob/master/LICENSE
+ * This file may not be copied, modified, propagated, or distributed
+ *    except according to the terms contained in the LICENSE file.
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "einfo.h"
+#include "rc-sysvinit.h"
+
+static void sysvinit_send_cmd(struct init_request *request)
+{
+	int fd;
+	char *p;
+	size_t bytes;
+	ssize_t r;
+
+	fd = open("/run/initctl", O_WRONLY|O_NONBLOCK|O_CLOEXEC|O_NOCTTY);
+	if (fd < 0) {
+		if (errno != ENOENT)
+			eerror("Failed to open initctl fifo: %s", strerror(errno));
+		return;
+	}
+	p = (char *) request;
+	bytes = sizeof(*request);
+	do {
+		r = write(fd, p, bytes);
+		if (r < 0) {
+			if ((errno == EAGAIN) || (errno == EINTR))
+				continue;
+			eerror("Failed to write to /run/initctl: %s", strerror(errno));
+			return;
+		}
+		p += r;
+		bytes -= r;
+	} while (bytes > 0);
+}
+
+void sysvinit_runlevel(char rl)
+{
+	struct init_request request;
+
+	if (!rl)
+		return;
+
+	request = (struct init_request) {
+		.magic = INIT_MAGIC,
+		.sleeptime = 0,
+		.cmd = INIT_CMD_RUNLVL,
+		.runlevel = rl,
+	};
+	sysvinit_send_cmd(&request);
+		return;
+}
+
+/*
+ *	Set environment variables in the init process.
+ */
+void sysvinit_setenv(char *name, char *value)
+{
+	struct init_request	request;
+	size_t nl;
+	size_t vl;
+
+	memset(&request, 0, sizeof(request));
+	request.magic = INIT_MAGIC;
+	request.cmd = INIT_CMD_SETENV;
+	nl = strlen(name);
+	if (value)
+		vl = strlen(value);
+else
+		vl = 0;
+
+	if (nl + vl + 3 >= (int)sizeof(request.i.data))
+		return -1;
+
+	memcpy(request.i.data, name, nl);
+	if (value) {
+		request.i.data[nl] = '=';
+		memcpy(request.i.data + nl + 1, value, vl);
+	}
+	sysvinit_send_cmd(&request);
+	return 0;
+}
